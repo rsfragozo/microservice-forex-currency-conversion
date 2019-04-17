9d77dce9abac93e28bc88ed70db80c9ab3bd75c2
Rodrigo Soares Fragozo <rodrigo.fragozo@gmail.com>
2019-04-17 23:43:36 +0200

    Initial commit

---
+++ src/main/java/com/atcoffeebreak/forex/currencyconversion/MicroserviceForexCurrencyConversionApplication.java
@@ -0,0 +1,13 @@
+package com.atcoffeebreak.forex.currencyconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceForexCurrencyConversionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceForexCurrencyConversionApplication.class, args);
+    }
+
+}
